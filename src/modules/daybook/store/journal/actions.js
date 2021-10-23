96b688c0b9e31369d3eeb9a2a4865d566830a4bd
Antony <toileok1@gmail.com>
2021-10-23 18:12:22 -0500

    vuex custom

---
+++ src/modules/daybook/store/journal/actions.js
@@ -0,0 +1,15 @@
+/*export const myAction = async ({ commit }) => {
+
+}*/
+
+export const loadEntries = async ({ commit }) => {
+
+}
+
+export const updateEntry = async ({ commint }) => {
+
+}
+
+export const createEntry = async ({ commint }) => {
+    
+}
---
+++ src/modules/daybook/store/journal/getters.js
@@ -0,0 +1,15 @@
+/*
+
+//trae informacion del state
+
+export const myGetters = ( state) => {
+    return state
+}*/
+
+export const getEntriesByTerm = ( state ) => {
+    
+}
+
+export const getEntriesById = ( state ) => {
+    
+}
